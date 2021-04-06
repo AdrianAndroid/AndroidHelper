961e1c91179a564d9e5498b35d9b564db9c7d7cb
flannery <18310579837@163.com>
2021-04-06 22:11:44 +0800

    软/硬解码

--- ffmpeg/src/main/cpp/native-lib.cpp
+++ ffmpeg/src/main/cpp/native-lib.cpp
@@ -6,13 +6,227 @@
 
 extern "C" {
 #include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h>
+#include <libavcodec/jni.h>
+#include <libswscale/swscale.h> //像素格式转换
 }
+
+#include <iostream>
+
+using namespace std;
+
+static double r2d(AVRational r) {
+    return r.num == 0 || r.den == 0 ? 0. : (double) r.num / (double) r.den;
+}
+
+long long GetNowMs()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    int sec = tv.tv_sec%360000;
+    long long t = sec*1000+tv.tv_usec/1000;
+    return t;
+}
+
+
+extern "C"
+JNIEXPORT
+jint JNI_OnLoad(JavaVM *vm,void *res)
+{
+    av_jni_set_java_vm(vm,0);
+    return JNI_VERSION_1_4;
+}
+
+
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_example_ffmpeg_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     std::string hello = "Hello from C++ ";
     hello += avcodec_configuration();
+    //初始化解封装
+    av_register_all();
+    //初始化网络
+    avformat_network_init();
+
+    avcodec_register_all();
+
+    //打开文件
+    AVFormatContext *ic = NULL;
+    char path[] = "/sdcard/test.mp4";
+    //char path[] = "/sdcard/video.flv";
+    int re = avformat_open_input(&ic,path,0,0);
+    if(re != 0)
+    {
+        LOGW("avformat_open_input failed!:%s",av_err2str(re));
+        return env->NewStringUTF(hello.c_str());
+    }
+    LOGW("avformat_open_input %s success!",path);
+    //获取流信息
+    re = avformat_find_stream_info(ic,0);
+    if(re != 0)
+    {
+        LOGW("avformat_find_stream_info failed!");
+    }
+    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams);
+
+    int fps = 0;
+    int videoStream = 0;
+    int audioStream = 1;
+
+    for(int i = 0; i < ic->nb_streams; i++)
+    {
+        AVStream *as = ic->streams[i];
+        if(as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
+        {
+            LOGW("视频数据");
+            videoStream = i;
+            fps = r2d(as->avg_frame_rate);
+
+            LOGW("fps = %d,width=%d height=%d codeid=%d pixformat=%d",fps,
+                 as->codecpar->width,
+                 as->codecpar->height,
+                 as->codecpar->codec_id,
+                 as->codecpar->format
+            );
+        }
+        else if(as->codecpar->codec_type ==AVMEDIA_TYPE_AUDIO )
+        {
+            LOGW("音频数据");
+            audioStream = i;
+            LOGW("sample_rate=%d channels=%d sample_format=%d",
+                 as->codecpar->sample_rate,
+                 as->codecpar->channels,
+                 as->codecpar->format
+            );
+        }
+    }
+    //ic->streams[videoStream];
+    //获取音频流信息
+    audioStream = av_find_best_stream(ic,AVMEDIA_TYPE_AUDIO,-1,-1,NULL,0);
+    LOGW("av_find_best_stream audioStream = %d",audioStream);
+    //////////////////////////////////////////////////////////
+    //打开视频解码器
+    //软解码器
+    AVCodec *codec = NULL;
+    //codec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
+    //硬解码
+    codec = avcodec_find_decoder_by_name("h264_mediacodec");
+    if(!codec)
+    {
+        LOGW("avcodec_find failed!");
+        return env->NewStringUTF(hello.c_str());
+    }
+    //解码器初始化
+    AVCodecContext *vc = avcodec_alloc_context3(codec);
+    // LOGW("streams timebase = %d/%d",ic->streams[videoStream]->time_base.num,ic->streams[videoStream]->time_base.den);
+    avcodec_parameters_to_context(vc,ic->streams[videoStream]->codecpar);
+    //vc->time_base = ic->streams[videoStream]->time_base;
+    // LOGW("vc timebase = %d",vc->time_base.den);
+    vc->thread_count = 8;
+    //打开解码器
+    re = avcodec_open2(vc,0,0);
+    //vc->time_base = ic->streams[videoStream]->time_base;
+    LOGW("vc timebase = %d/ %d",vc->time_base.num,vc->time_base.den);
+    if(re != 0)
+    {
+        LOGW("avcodec_open2 video failed!");
+        return env->NewStringUTF(hello.c_str());
+    }
+
+    //////////////////////////////////////////////////////////
+    //打开音频解码器
+    //软解码器
+    AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
+    //硬解码
+    //codec = avcodec_find_decoder_by_name("h264_mediacodec");
+    if(!acodec)
+    {
+        LOGW("avcodec_find failed!");
+        return env->NewStringUTF(hello.c_str());
+    }
+    //解码器初始化
+    AVCodecContext *ac = avcodec_alloc_context3(acodec);
+    avcodec_parameters_to_context(ac,ic->streams[audioStream]->codecpar);
+    ac->thread_count = 8;
+    //打开解码器
+    re = avcodec_open2(ac,0,0);
+    if(re != 0)
+    {
+        LOGW("avcodec_open2  audio failed!");
+        return env->NewStringUTF(hello.c_str());
+    }
+    //读取帧数据
+    AVPacket *pkt = av_packet_alloc();
+    AVFrame *frame = av_frame_alloc();
+    long long start = GetNowMs();
+    int frameCount = 0;
+    for(;;)
+    {
+        long long end = GetNowMs();
+        if(end - start >= 3000)
+        {
+            LOGW("now decode fps is %d", frameCount/3);
+            start = end;
+            frameCount = 0;
+        }
+        int re = av_read_frame(ic,pkt);
+        if(re != 0)
+        {
+
+            LOGW("读取到结尾处!");
+            int pos = 20 * r2d(ic->streams[videoStream]->time_base);
+            av_seek_frame(ic,videoStream,pos,AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME );
+            continue;
+        }
+        //只测试视频
+        /*if(pkt->stream_index !=videoStream)
+        {
+            continue;
+        }*/
+        //LOGW("stream = %d size =%d pts=%lld flag=%d",
+        //     pkt->stream_index,pkt->size,pkt->pts,pkt->flags
+        //);
+
+        AVCodecContext *cc = vc;
+        if(pkt->stream_index == audioStream)
+            cc=ac;
+
+        //发送到线程中解码
+        re = avcodec_send_packet(cc,pkt);
+        //清理
+        int p = pkt->pts;
+        av_packet_unref(pkt);
+
+        if(re != 0)
+        {
+            LOGW("avcodec_send_packet failed!");
+            continue;
+        }
+        for(;;)
+        {
+            re = avcodec_receive_frame(cc,frame);
+            if(re !=0)
+            {
+                //LOGW("avcodec_receive_frame failed!");
+                break;
+            }
+            //LOGW("avcodec_receive_frame %lld",frame->pts);
+            if(cc == vc)
+            {
+                frameCount++;
+            }
+        }
+
+        //////////////////////
+
+
+    }
+
+
+
+    //关闭上下文
+    avformat_close_input(&ic);
     return env->NewStringUTF(hello.c_str());
 }
 
