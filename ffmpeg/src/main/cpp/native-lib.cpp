7760a3378a8391691630d62ebe19ed7403110d4b
flannery <18310579837@163.com>
2021-04-07 10:39:14 +0800

    重启了模拟器，就可以用了，奇怪

--- ffmpeg/src/main/cpp/native-lib.cpp
+++ ffmpeg/src/main/cpp/native-lib.cpp
@@ -9,6 +9,7 @@ extern "C" {
 #include <libavformat/avformat.h>
 #include <libavcodec/jni.h>
 #include <libswscale/swscale.h> //像素格式转换
+#include <libswresample/swresample.h>
 }
 
 #include <iostream>
@@ -19,21 +20,19 @@ static double r2d(AVRational r) {
     return r.num == 0 || r.den == 0 ? 0. : (double) r.num / (double) r.den;
 }
 
-long long GetNowMs()
-{
+long long GetNowMs() {
     struct timeval tv;
     gettimeofday(&tv, NULL);
-    int sec = tv.tv_sec%360000;
-    long long t = sec*1000+tv.tv_usec/1000;
+    int sec = tv.tv_sec % 360000;
+    long long t = sec * 1000 + tv.tv_usec / 1000;
     return t;
 }
 
 
 extern "C"
 JNIEXPORT
-jint JNI_OnLoad(JavaVM *vm,void *res)
-{
-    av_jni_set_java_vm(vm,0);
+jint JNI_OnLoad(JavaVM *vm, void *res) {
+    av_jni_set_java_vm(vm, 0);
     return JNI_VERSION_1_4;
 }
 
@@ -53,45 +52,39 @@ Java_com_example_ffmpeg_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
 
     //打开文件
     AVFormatContext *ic = NULL;
-    char path[] = "/sdcard/test.mp4";
+    char path[] = "/sdcard/1080.mp4";
     //char path[] = "/sdcard/video.flv";
-    int re = avformat_open_input(&ic,path,0,0);
-    if(re != 0)
-    {
-        LOGW("avformat_open_input failed!:%s",av_err2str(re));
+    int re = avformat_open_input(&ic, path, 0, 0);
+    if (re != 0) {
+        LOGW("avformat_open_input failed!:%s", av_err2str(re));
         return env->NewStringUTF(hello.c_str());
     }
-    LOGW("avformat_open_input %s success!",path);
+    LOGW("avformat_open_input %s success!", path);
     //获取流信息
-    re = avformat_find_stream_info(ic,0);
-    if(re != 0)
-    {
+    re = avformat_find_stream_info(ic, 0);
+    if (re != 0) {
         LOGW("avformat_find_stream_info failed!");
     }
-    LOGW("duration = %lld nb_streams = %d",ic->duration,ic->nb_streams);
+    LOGW("duration = %lld nb_streams = %d", ic->duration, ic->nb_streams);
 
     int fps = 0;
     int videoStream = 0;
     int audioStream = 1;
 
-    for(int i = 0; i < ic->nb_streams; i++)
-    {
+    for (int i = 0; i < ic->nb_streams; i++) {
         AVStream *as = ic->streams[i];
-        if(as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO)
-        {
+        if (as->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             LOGW("视频数据");
             videoStream = i;
             fps = r2d(as->avg_frame_rate);
 
-            LOGW("fps = %d,width=%d height=%d codeid=%d pixformat=%d",fps,
+            LOGW("fps = %d,width=%d height=%d codeid=%d pixformat=%d", fps,
                  as->codecpar->width,
                  as->codecpar->height,
                  as->codecpar->codec_id,
                  as->codecpar->format
             );
-        }
-        else if(as->codecpar->codec_type ==AVMEDIA_TYPE_AUDIO )
-        {
+        } else if (as->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
             LOGW("音频数据");
             audioStream = i;
             LOGW("sample_rate=%d channels=%d sample_format=%d",
@@ -103,35 +96,38 @@ Java_com_example_ffmpeg_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     }
     //ic->streams[videoStream];
     //获取音频流信息
-    audioStream = av_find_best_stream(ic,AVMEDIA_TYPE_AUDIO,-1,-1,NULL,0);
-    LOGW("av_find_best_stream audioStream = %d",audioStream);
+    audioStream = av_find_best_stream(ic, AVMEDIA_TYPE_AUDIO, -1, -1, NULL, 0);
+    LOGW("av_find_best_stream audioStream = %d", audioStream);
     //////////////////////////////////////////////////////////
     //打开视频解码器
     //软解码器
-    AVCodec *codec = NULL;
-    //codec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
+    AVCodec *codec = avcodec_find_decoder(ic->streams[videoStream]->codecpar->codec_id);
     //硬解码
     codec = avcodec_find_decoder_by_name("h264_mediacodec");
-    if(!codec)
-    {
+    if (!codec) {
         LOGW("avcodec_find failed!");
         return env->NewStringUTF(hello.c_str());
+    } else {
+        LOGW("avcodec_find success!");
     }
+    LOGW("avcodec_alloc_context3 before!");
     //解码器初始化
     AVCodecContext *vc = avcodec_alloc_context3(codec);
-    // LOGW("streams timebase = %d/%d",ic->streams[videoStream]->time_base.num,ic->streams[videoStream]->time_base.den);
-    avcodec_parameters_to_context(vc,ic->streams[videoStream]->codecpar);
-    //vc->time_base = ic->streams[videoStream]->time_base;
-    // LOGW("vc timebase = %d",vc->time_base.den);
+    avcodec_parameters_to_context(vc, ic->streams[videoStream]->codecpar);
+    LOGW("avcodec_alloc_context3 after!");
+
     vc->thread_count = 8;
     //打开解码器
-    re = avcodec_open2(vc,0,0);
+    LOGW("avcodec_open2 before! -->%d,%d,%d", vc->thread_count,vc->width, vc->height);
+    re = avcodec_open2(vc, 0, 0);
+    LOGW("avcodec_open2 after! -->%d", re);
     //vc->time_base = ic->streams[videoStream]->time_base;
-    LOGW("vc timebase = %d/ %d",vc->time_base.num,vc->time_base.den);
-    if(re != 0)
-    {
+    LOGW("vc timebase = %d/ %d", vc->time_base.num, vc->time_base.den);
+    if (re != 0) {
         LOGW("avcodec_open2 video failed!");
         return env->NewStringUTF(hello.c_str());
+    } else {
+        LOGW("avcodec_open2 video success!");
     }
 
     //////////////////////////////////////////////////////////
@@ -140,43 +136,71 @@ Java_com_example_ffmpeg_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
     AVCodec *acodec = avcodec_find_decoder(ic->streams[audioStream]->codecpar->codec_id);
     //硬解码
     //codec = avcodec_find_decoder_by_name("h264_mediacodec");
-    if(!acodec)
-    {
+    if (!acodec) {
         LOGW("avcodec_find failed!");
         return env->NewStringUTF(hello.c_str());
+    } else {
+        LOGW("avcodec_find success!");
     }
-    //解码器初始化
+
+    LOGW("avcodec_open2 audio before before!");
+    //音频解码器初始化
     AVCodecContext *ac = avcodec_alloc_context3(acodec);
-    avcodec_parameters_to_context(ac,ic->streams[audioStream]->codecpar);
+    avcodec_parameters_to_context(ac, ic->streams[audioStream]->codecpar);
     ac->thread_count = 8;
+    LOGW("avcodec_open2 audio before!");
     //打开解码器
-    re = avcodec_open2(ac,0,0);
-    if(re != 0)
-    {
+    re = avcodec_open2(ac, 0, 0);
+    if (re != 0) {
         LOGW("avcodec_open2  audio failed!");
         return env->NewStringUTF(hello.c_str());
+    } else {
+        LOGW("avcodec_open2 audio success!");
     }
     //读取帧数据
     AVPacket *pkt = av_packet_alloc();
     AVFrame *frame = av_frame_alloc();
     long long start = GetNowMs();
     int frameCount = 0;
-    for(;;)
-    {
-        long long end = GetNowMs();
-        if(end - start >= 3000)
-        {
-            LOGW("now decode fps is %d", frameCount/3);
-            start = end;
+
+
+    //初始化像素格式转换的上下文
+    SwsContext *vctx = NULL;
+    int outWidth = 1280;
+    int outHeight = 720;
+    char *rgb = new char[1920 * 1080 * 4];
+    char *pcm = new char[48000 * 4 * 2];
+
+    //音频重采样上下文初始化
+    SwrContext *actx = swr_alloc();
+    actx = swr_alloc_set_opts(actx,
+                              av_get_default_channel_layout(2),
+                              AV_SAMPLE_FMT_S16, ac->sample_rate,
+                              av_get_default_channel_layout(ac->channels),
+                              ac->sample_fmt, ac->sample_rate,
+                              0, 0);
+    re = swr_init(actx);
+    if (re != 0) {
+        LOGW("swr_init failed!");
+    } else {
+        LOGW("swr_init success!");
+    }
+
+
+    for (;;) {
+        //超过三秒
+        if (GetNowMs() - start >= 3000) {
+            LOGW("now decode fps is %d", frameCount / 3);
+            start = GetNowMs();
             frameCount = 0;
         }
-        int re = av_read_frame(ic,pkt);
-        if(re != 0)
-        {
+
+        int re = av_read_frame(ic, pkt);
+        if (re != 0) {
 
             LOGW("读取到结尾处!");
             int pos = 20 * r2d(ic->streams[videoStream]->time_base);
-            av_seek_frame(ic,videoStream,pos,AVSEEK_FLAG_BACKWARD|AVSEEK_FLAG_FRAME );
+            av_seek_frame(ic, videoStream, pos, AVSEEK_FLAG_BACKWARD | AVSEEK_FLAG_FRAME);
             continue;
         }
         //只测试视频
@@ -189,39 +213,77 @@ Java_com_example_ffmpeg_MainActivity_stringFromJNI(JNIEnv *env, jobject thiz) {
         //);
 
         AVCodecContext *cc = vc;
-        if(pkt->stream_index == audioStream)
-            cc=ac;
+        if (pkt->stream_index == audioStream)
+            cc = ac;
 
         //发送到线程中解码
-        re = avcodec_send_packet(cc,pkt);
+        re = avcodec_send_packet(cc, pkt);
         //清理
         int p = pkt->pts;
         av_packet_unref(pkt);
 
-        if(re != 0)
-        {
+        if (re != 0) {
             LOGW("avcodec_send_packet failed!");
             continue;
         }
-        for(;;)
-        {
-            re = avcodec_receive_frame(cc,frame);
-            if(re !=0)
-            {
+
+
+        for (;;) {
+            re = avcodec_receive_frame(cc, frame);
+            if (re != 0) {
                 //LOGW("avcodec_receive_frame failed!");
                 break;
             }
             //LOGW("avcodec_receive_frame %lld",frame->pts);
-            if(cc == vc)
-            {
+            //如果是视频帧
+            if (cc == vc) {
                 frameCount++;
+                vctx = sws_getCachedContext(vctx,
+                                            frame->width,
+                                            frame->height,
+                                            (AVPixelFormat) frame->format,
+                                            outWidth,
+                                            outHeight,
+                                            AV_PIX_FMT_RGBA,
+                                            SWS_FAST_BILINEAR,
+                                            0, 0, 0
+                );
+                if (!vctx) {
+                    LOGW("sws_getCachedContext failed!");
+                } else {
+                    uint8_t *data[AV_NUM_DATA_POINTERS] = {0};
+                    data[0] = (uint8_t *) rgb;
+                    int lines[AV_NUM_DATA_POINTERS] = {0};
+                    lines[0] = outWidth * 4;
+                    int h = sws_scale(vctx,
+                                      (const uint8_t **) frame->data,
+                                      frame->linesize, 0,
+                                      frame->height,
+                                      data, lines);
+                    LOGW("sws_scale = %d", h);
+                }
+
+            } else //音频
+            {
+                uint8_t *out[2] = {0};
+                out[0] = (uint8_t *) pcm;
+
+                //音频重采样
+                int len = swr_convert(actx, out,
+                                      frame->nb_samples,
+                                      (const uint8_t **) frame->data,
+                                      frame->nb_samples);
+                LOGW("swr_convert = %d", len);
             }
+
         }
 
         //////////////////////
 
 
     }
+    delete rgb;
+    delete pcm;
 
 
 
