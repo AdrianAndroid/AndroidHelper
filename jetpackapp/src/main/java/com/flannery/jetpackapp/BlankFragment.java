5f957bea691ecc6b3b65551896584a9a5c94e5eb
flannery <18310579837@163.com>
2021-05-08 08:31:50 +0800

    添加bulubulu

--- jetpackapp/src/main/java/com/flannery/jetpackapp/BlankFragment.java
+++ jetpackapp/src/main/java/com/flannery/jetpackapp/BlankFragment.java
@@ -2,11 +2,15 @@ package com.flannery.jetpackapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RestrictTo;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,9 +28,14 @@ public class BlankFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    @RestrictTo(RestrictTo.Scope.LIBRARY_GROUP)
+    @NonNull
+    AtomicReference<Object> mInternalScopeRef = new AtomicReference<>();
+
     public BlankFragment() {
         // Required empty public constructor
     }
+
 
     /**
      * Use this factory method to create a new instance of
