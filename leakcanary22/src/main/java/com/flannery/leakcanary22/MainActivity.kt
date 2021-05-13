4d03995c24b1ed5f47004fe8d8a769798ce414f8
flannery <18310579837@163.com>
2021-05-13 17:26:45 +0800

    aidl

--- leakcanary22/src/main/java/com/flannery/leakcanary22/MainActivity.kt
+++ leakcanary22/src/main/java/com/flannery/leakcanary22/MainActivity.kt
@@ -1,8 +1,18 @@
 package com.flannery.leakcanary22
 
+import android.app.Service
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.*
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import leakcanary.LeakCanary
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
+import rx.Observable
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,9 +20,84 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        synchronized(this){
+//            Thread.sleep(10000)
+//        }
+    }
+
+    fun test2() {
+        //IBookManager.Stub
+        val m = Messenger(Handler(Looper.getMainLooper()));
+        //m.binder
+
     }
 
     fun test() {
+//        EventBus.builder().build().post("");
+//        EventBus.getDefault().post("")
+        EventBus.getDefault().register(this)
+        EventBus.getDefault().unregister(this)
+        EventBus.getDefault().postSticky("")
 
+    }
+
+    // https://www.jianshu.com/p/239a866cf85d
+    // startService
+    // onCreate - onStartCommand - onDestroy
+    // bindService
+    // onCreate - onBind - onUnbind - onDestroy
+
+    fun _startService() {
+        startService(Intent(this, MService::class.java))
+//        I/System.out: MService()
+//        I/System.out: attachBaseContext
+//        I/System.out: onCreate
+//        I/System.out: onStartCommand
+//        I/System.out: onStart
+
+    }
+
+    fun _bindService() {
+        bindService(Intent(this, MService::class.java), object :ServiceConnection{
+            override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+                println("onServiceConnected")
+                var asInterface = IBookManager.Stub.asInterface(service)
+            }
+
+            override fun onServiceDisconnected(name: ComponentName?) {
+                println("onServiceDisconnected")
+            }
+        }, 0)
+    }
+
+    fun clickTest(view: View) {
+        //testObserver()
+        //test()
+        //testObserver()
+        _bindService()
+    }
+
+
+
+    @Subscribe(threadMode = ThreadMode.MAIN)
+    private fun testObserver() {
+        Observable
+            .just("lfkdsk", "just-we")
+            .map {
+                println("map1 ${Thread.currentThread().name}")
+            }
+            .map {
+                println("map2 ${Thread.currentThread().name}")
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .map {
+                println("map3 ${Thread.currentThread().name}")
+            }
+            .map {
+                println("map4 ${Thread.currentThread().name}")
+            }
+            .subscribe()
     }
 }
