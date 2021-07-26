8ec8f6ac41953cb982826f4b7297b384ecafb6c4
zhaojian2 <zhaojian2@joyy.sg>
2021-07-26 15:29:39 +0800

    添加了一个dynamic feature

--- gradledemo/gradledownload/src/main/java/com/fannery/gradledownload/MainActivity.kt
+++ gradledemo/gradledownload/src/main/java/com/fannery/gradledownload/MainActivity.kt
@@ -2,8 +2,11 @@ package com.fannery.gradledownload
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import java.lang.StringBuilder
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- leetcode/src/main/java/com/imooc/router/mapping/generated/RouterMapping.java
+++ leetcode/src/main/java/com/imooc/router/mapping/generated/RouterMapping.java
@@ -2,8 +2,8 @@ package com.imooc.router.mapping.generated;
 
 /**
  * Time:2021/7/23 16:57
- * Author:
- * Description:
+ * Author: flannery
+ * Description: 这个类没用
  */
 public class RouterMapping {
 
