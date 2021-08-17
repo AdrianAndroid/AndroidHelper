3d0b2e17f7f9b89b0163468eec96480b2511d547
zhaojian2 <zhaojian2@joyy.sg>
2021-08-17 11:42:38 +0800

    find_ar

--- cpp/common/Node.cpp
+++ cpp/common/Node.cpp
@@ -12,3 +12,12 @@ Node *find_all(Node *head, const double &val) {
     }
     return nullptr;
 }
+
+typedef double *iterator;
+
+iterator find_ar(iterator ar, int n, const double &val) {
+    for (int i = 0; i < n; i++, ar++) {
+        if (*ar == val) return ar;
+    }
+    return nullptr;
+}
