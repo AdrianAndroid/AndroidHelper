18a9a687debd275eca7fc8f4bc9bdc32230c4ce5
zhaojian2 <zhaojian2@joyy.sg>
2021-09-07 14:47:01 +0800

    添加TaskDispatcher

---
+++ android/TaskDispatcher/dispatcher/src/main/java/com/joyy/dispatcher/DelayInitDispatcher.java
@@ -0,0 +1,42 @@
+package com.joyy.dispatcher;
+
+import android.os.Looper;
+import android.os.MessageQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * Time:2021/9/7 13:45
+ * Author: flannery
+ * Description: 延迟初始化分发器
+ */
+
+public class DelayInitDispatcher {
+
+    private Queue<Task> mDelayTasks = new LinkedList<>();
+
+    /**
+     * 使用IdleHandler 可以实现在app在空闲时执行任务
+     */
+    private MessageQueue.IdleHandler mIdleHandler = new MessageQueue.IdleHandler() {
+        @Override
+        public boolean queueIdle() {
+            if(mDelayTasks.size()>0){
+                Task task = mDelayTasks.poll();
+                new DispatchRunnable(task).run();
+            }
+            return !mDelayTasks.isEmpty();
+        }
+    };
+
+    public DelayInitDispatcher addTask(Task task){
+        mDelayTasks.add(task);
+        return this;
+    }
+
+    public void start(){
+        Looper.myQueue().addIdleHandler(mIdleHandler);
+    }
+
+}
