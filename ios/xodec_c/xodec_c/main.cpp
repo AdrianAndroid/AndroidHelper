50c1f86b5cd0af8c138155132a768786db2e0d80
zhaojian2 <zhaojian2@joyy.com>
2021-11-08 18:07:26 +0800

    dddd

--- ios/xodec_c/xodec_c/main.cpp
+++ ios/xodec_c/xodec_c/main.cpp
@@ -6,9 +6,20 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <list>
+
+using namespace std;
+
+void hello();
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout<<"Hello, World!\n"<<std::endl;
+    hello();
     return 0;
+}
+
+void hello() {
+    cout << "Hello 111f" << endl;
 }
